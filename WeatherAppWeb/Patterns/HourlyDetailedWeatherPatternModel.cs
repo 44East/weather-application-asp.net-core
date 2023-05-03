2687c833127ac98950c79720c36a05d4926186b0
44East <antswert@gmail.com>
2023-05-03 18:52:16 +0300

    Finished the detailed hourl pattern model

--- WeatherAppWeb/Patterns/HourlyDetailedWeatherPatternModel.cs
+++ WeatherAppWeb/Patterns/HourlyDetailedWeatherPatternModel.cs
@@ -15,28 +15,39 @@ namespace WeatherAppWeb.Patterns
         /// <param name="cityInfo">The basic information of the city.</param>
         public HourlyDetailedWeatherPatternModel(HourlyDetailedForecast forecast, RootBasicCityInfo cityInfo)
         {
+            /*--------------------------------------------------------------------- Location */
             CityName = cityInfo.EnglishName;
             CountryName = cityInfo.Country.EnglishName;
             CityKey = cityInfo.Key;
-
+            /*--------------------------------------------------------------------- Temperature */
+            TemperatureUnit = forecast.Temperature.Unit;
             Temperature = forecast.Temperature.Value;
+            DewPointTemperature = forecast.DewPoint.Value;
             RealFeelTemperature = forecast.RealFeelTemperature.Value;
+            TempratureFeelings = forecast.RealFeelTemperature.Phrase;
             //RealFeelTemperature = forecast.RealFeelTemperatureShade.Value; 
-
+            /*--------------------------------------------------------------------- Wind */
             WindSpeed = forecast.Wind.Speed.Value;
             WindUnitOfMeasure = forecast.Wind.Speed.Unit;
             WindDirection = GetTheWindDirection(forecast.Wind.Direction.English);
             WindGustSpeed = forecast.WindGust.Speed.Value;
-
+            /*--------------------------------------------------------------------- Precepitation */
             HasPrecipitation = forecast.HasPrecipitation;
             PrecipitationProbability = forecast.PrecipitationProbability;
             PrecipitationType = forecast.PrecipitationType;
-
-            IsDayLight = forecast.IsDaylight;
-            
-            ForecastMessage = forecast.IconPhrase;
-            
+            //If the forecast has precipitation, it checks the type of the precipitation and sets the value of total liquid. If precipitation doesn't exist it sets total value = 0; 
+            TotalPrecipitationLiquid = forecast.HasPrecipitation ? GetTheTotalLiquid(forecast) : 0;
+            TypeOfPrecipitationPropability = GetTypeOfPrecipitationPropability(forecast);
+            /*--------------------------------------------------------------------- Sun */
             TimeOfNow = forecast.IsDaylight ? "day" : "night";
+            IsDayLight = forecast.IsDaylight;
+            UVIndex = forecast.UVIndex;
+            UVIndexText = forecast.UVIndexText;
+            SolarIrradiance = forecast.SolarIrradiance.Value;
+            SolarIrradianceUnit = forecast.SolarIrradiance.Unit;
+            /*---------------------------------------------------------------------*/
+
+            ForecastMessage = forecast.IconPhrase;
             WeatherIcon = forecast.WeatherIcon.ToString() + ".png";
         }
 
@@ -64,6 +75,18 @@ namespace WeatherAppWeb.Patterns
         /// The real feel temperature value.
         /// </summary>
         public double RealFeelTemperature { get; init; }
+        /// <summary>
+        /// The dew point temperature 
+        /// </summary>
+        public double DewPointTemperature { get; init; }
+        /// <summary>
+        /// The unit of measurement
+        /// </summary>
+        public string TemperatureUnit { get; init; }
+        /// <summary>
+        /// Message about feeling of the temperature
+        /// </summary>
+        public string TempratureFeelings { get; init; }
         #endregion
 
         #region Wind 
@@ -131,7 +154,6 @@ namespace WeatherAppWeb.Patterns
 
         #endregion
 
-
         #region Precipitation
         /// <summary>
         /// Indicates whether it has precipitation or not
@@ -145,29 +167,103 @@ namespace WeatherAppWeb.Patterns
         /// <summary>
         /// Declare type of precipitation (Rain, Snow, Ice or mixed)
         /// </summary>
-        public string PrecipitationType { get; set; }
+        public string PrecipitationType { get; init; }
+        /// <summary>
+        /// Rounded value in specified units
+        /// </summary>
+        public double TotalPrecipitationLiquid { get; init; }
+        /// <summary>
+        /// Sets the total value of the precipitation liquid and unit type of measurement 
+        /// </summary>
+        /// <param name="forecast">The current forecast instance</param>
+        /// <returns>A <see cref="double"/> value of precipitation liquid </returns>
+        private double GetTheTotalLiquid(HourlyDetailedForecast forecast)
+        {
+            
+            switch (forecast.PrecipitationType?.ToLower()) 
+            {
+                case "snow":
+                    LiquidUnitOfMeasure = forecast.Snow.Unit;
+                    return forecast.Snow.Value;
+                case "rain": 
+                    LiquidUnitOfMeasure = forecast.Rain.Unit;
+                    return forecast.Rain.Value;
+                case "ice": 
+                    LiquidUnitOfMeasure = forecast.Ice.Unit;
+                    return forecast.Ice.Value;
+                default:
+                    LiquidUnitOfMeasure = forecast.TotalLiquid.Unit;
+                    return forecast.TotalLiquid.Value;                     
+            }
+        }
+        /// <summary>
+        /// The unit of measurement
+        /// </summary>
+        public string LiquidUnitOfMeasure { get; private set; }
+        /// <summary>
+        /// Obtain the type of precipitation if available.
+        /// </summary>
+        /// <param name="forecast">Current forecast instance</param>
+        /// <returns>Type of precipitation</returns>
+        private string GetTypeOfPrecipitationPropability(HourlyDetailedForecast forecast)
+        {
+            if (forecast.IceProbability > 0)
+            {
+                return "Ice rain";
+            }
+            else if (forecast.RainProbability > 0)
+            {
+                return "Rain";
+            }
+            else if (forecast.SnowProbability > 0)
+            {
+                return "Snow";
+            }
+            else
+                return string.Empty;
+        }
+        /// <summary>
+        /// The type of precipitation
+        /// </summary>
+        public string TypeOfPrecipitationPropability { get; set; }
+
+
+        #endregion
+
+        #region Sun
+        /// <summary>
+        /// Value of the strength of the ultraviolet radiation from the sun
+        /// </summary>
+        public int UVIndex { get; init; }
+        /// <summary>
+        /// A text status of the UV Index  
+        /// </summary>
+        public string UVIndexText { get; init; }
+        /// <summary>
+        /// Indicates whether it is the daylight or not
+        /// </summary>
+        public bool IsDayLight { get; init; }
+        /// <summary>
+        /// Gets the time of a day.
+        /// </summary>
+        public string TimeOfNow { get; init; }
+        /// <summary>
+        /// The value of the solar irradiance in W/m²
+        /// </summary>
+        public double SolarIrradiance { get; set; }
+        /// <summary>
+        /// Type of the measurement unit of the solar irradiance
+        /// </summary>
+        public string SolarIrradianceUnit { get; set; }
         #endregion
 
 
-        /// <summary>
-        /// Indicates whether it is daylight or not
-        /// </summary>
-        public bool IsDayLight { get; init; }
-
-        
-
-        
 
         /// <summary>
         /// The forecast message with the weather review.
         /// </summary>
         public string ForecastMessage { get; init; }
-
-        /// <summary>
-        /// Gets the time of day.
-        /// </summary>
-        public string TimeOfNow { get; init; }
-
+        
         /// <summary>
         /// Contains the weather icon file name.
         /// </summary>
